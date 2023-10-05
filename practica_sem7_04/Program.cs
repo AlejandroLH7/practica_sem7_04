f20ab208b4cc0ad2ef9c157b40762d9a7d5f30a4
audio <audio@LAPTOP-A2DKNN9M>
2023-10-04 22:39:53 -0500

    Agregar archivos de proyecto.

---
+++ practica_sem7_04/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practica_sem7_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine
+                ("Ingresa un numero: ");
+            Console.WriteLine("==================");
+            int num = int.Parse(Console.ReadLine());
+            for (int i = 0; i < num; i++)
+            {
+                for (int j = 0; j < 2 * num; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+                Console.ReadKey();
+            }
+        }
+    }
+}
